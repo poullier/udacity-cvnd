136d161de95de4d90b6bf93e9e18d6123e1854d7
David Poullier <dpoullier@gmail.com>
2019-04-08 16:25:15 +0200

    Image caption project completed

--- P2_Image_Captioning/model.py
+++ P2_Image_Captioning/model.py
@@ -23,11 +23,44 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+        super(DecoderRNN, self).__init__()
+
+        # convert words into a vector
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+
+        # lstm no dropout, one layer and 512 embed size
+        self.lstm = nn.LSTM(input_size=embed_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
+
+        # convert the hidden state output dimension to the vocab_size
+        self.linear = nn.Linear(hidden_size, vocab_size)
     
     def forward(self, features, captions):
-        pass
+        # remove end words in each caption
+        embeds = self.embedding(captions[:, :-1])
+
+        # insert a dimension of size one at position 1
+        features = features.unsqueeze(1)
+
+        # concat features and embeds
+        inputs = torch.cat((features, embeds), 1)
+
+        # pass to lstm
+        hidden, states = self.lstm(inputs)
+        output = self.linear(hidden)
+        return output
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        words = []
+        for i in range(max_len):
+            hidden, states = self.lstm(inputs, states)
+            outputs = self.linear(hidden.squeeze(1))
+            idx = outputs.argmax(1)
+            word = idx.item()
+            words.append(word)
+            # reach end
+            if word == 1:
+                break
+            # use predicted word as input of the next iteration
+            inputs = self.embedding(idx.unsqueeze(1))
+        return words
